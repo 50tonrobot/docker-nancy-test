b3128fa2a9cbfecc87c17cb2e08414620205ec78
50tonrobot <50tonrobot@gmail.com>
2017-03-22 01:13:29 -0400

    initial commit

---
+++ NancyTest.test/Test.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+using NancyTest;
+namespace NancyTest.test
+{
+	[TestFixture()]
+	public class Test
+	{
+		[Test()]
+		public void TestCase()
+		{
+			Program myProgram = new Program();
+			Assert.IsTrue(myProgram.TestMe());
+		}
+	}
+}
---
+++ NancyTest/IndexModule.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Nancy;
+
+public class IndexModule : NancyModule
+{
+	public IndexModule()
+	{
+		Get["/"] = _ => "Nancy: Hello World";
+		Get["/os"] = _ => Environment.OSVersion.ToString();
+	}
+}
