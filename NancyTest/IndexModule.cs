fefa7c2d31271ff3f7b51025e03ebbae1222e1ea
50tonrobot <50tonrobot@gmail.com>
2017-03-24 02:41:54 -0400

    Content change

--- NancyTest/IndexModule.cs
+++ NancyTest/IndexModule.cs
@@ -5,7 +5,7 @@ public class IndexModule : NancyModule
 {
 	public IndexModule()
 	{
-		Get["/"] = _ => "Nancy: Hello World I am new";
+		Get["/"] = _ => "Nancy: Hello World What What";
 		Get["/os"] = _ => Environment.OSVersion.ToString();
 	}
 }
